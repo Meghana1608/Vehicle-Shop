e16b309c70f46f52ce8c34d04dfcb0367a58265b
Meghana1608 <66739220+Meghana1608@users.noreply.github.com>
2020-08-04 10:28:51 +0530

    printing statement

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,9 +9,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 //import static sample.LoadingDataToTableView.Welcome;
 import static sample.digital_clock.backupDataWithDatabase;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -143,7 +154,59 @@ public class Controller {
                 connection.close();
             }
         }
+    }
 
 
+    public static void export_excel(String query, String path) throws Exception {
+
+        Connection con = null;
+        try {
+            con = DBConnection.connect();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            XSSFWorkbook wb = new XSSFWorkbook();
+            XSSFSheet sheet = wb.createSheet("Items details");
+
+            XSSFCellStyle style = wb.createCellStyle();
+            style.setFillForegroundColor(IndexedColors.CORNFLOWER_BLUE.getIndex());
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+            XSSFRow header = sheet.createRow(0);
+
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                String colname = rs.getMetaData().getColumnName(i + 1);
+                header.createCell(i).setCellValue(colname.toUpperCase());
+                header.getCell(i).setCellStyle(style);
+            }
+
+
+            int index = 1;
+            while (rs.next()) {
+                XSSFRow row = sheet.createRow(index);
+                for (int j = 0; j < rs.getMetaData().getColumnCount(); j++) {
+                    row.createCell(j).setCellValue(rs.getString(j + 1));
+                }
+
+                index++;
+            }
+
+            FileOutputStream fileOut = new FileOutputStream(path);
+            wb.write(fileOut);
+            fileOut.close();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("Details Exported to excel sheet and stored in " + path);
+            alert.showAndWait();
+
+            ps.close();
+            rs.close();
+            con.close();
+
+            Desktop dt = Desktop.getDesktop();
+            dt.open(new File(path));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/sample/Home.java
+++ src/sample/Home.java
@@ -7,17 +7,43 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import sun.security.util.Password;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Home implements Initializable {
     public ComboBox comboo;
     public AnchorPane employee_pane;
+    public TextField employee_id;
+    public TextField employee_name;
+    public TextField employee_number;
+    public TextField password;
+    public Pane view_reg_pane;
+    public Pane edit_pane;
+    public TextField employee_Id;
+    public TextField email_id;
+    public TextField m_no;
+    public TextField address;
+    public TextField old_username;
+    public TextField new_username;
+    public TextField c_password;
+    Connection connection = null;
+    public static String idArray = "";
+    public static int incidArray;
 
     public void perform(ActionEvent actionEvent) throws IOException {
         String string1=comboo.getValue().toString();
@@ -56,7 +82,6 @@ public class Home implements Initializable {
             case "Exit" :
                 Platform.exit();
                 break;
-           // System.out.println("bg");
             //4
         }
     }
@@ -74,5 +99,189 @@ public class Home implements Initializable {
         employee_pane.setVisible(true);
 
 
+    }
+
+    public void create_id(MouseEvent mouseEvent) {
+        int i = 0;
+        try {
+            connection = DBConnection.getConnection();
+            String empIdIs = "";
+            Statement stm1 = connection.createStatement();
+
+            ResultSet rs = stm1.executeQuery("select max(e_id) as id from employee");
+            while (rs.next()) {
+                empIdIs = rs.getString("id");
+                i++;
+            }
+            if (empIdIs != null && i > 0) {
+
+                incidArray = (Integer.parseInt(empIdIs) + 1);
+                employee_id.setText(String.valueOf(incidArray));
+
+            } else {
+                employee_id.setText(String.valueOf(1));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void add_emp(ActionEvent actionEvent) throws Exception {
+        connection = DBConnection.getConnection();
+        String query = "select username from login";
+        ResultSet rs = connection.createStatement().executeQuery(query);
+        ArrayList<String> name = new ArrayList();
+        boolean employee_found = false;
+        while (rs.next()) {
+            name.add(rs.getString(1));
+        }
+
+        for (int i = 0; i < name.size(); i++) {
+            if (name.get(i).equalsIgnoreCase(employee_name.getText().trim())) {
+                employee_found = true;
+                break;
+            }
+        }
+
+        if (employee_name.getText().trim().isEmpty()) {
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setContentText("PLEASE ENTER EMPLOYEE NAME");
+            alert1.showAndWait();}
+
+//        } else if (!Password.getText().trim().equals(Confpassword.getText().trim())) {
+//            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+//            alert1.setContentText("PASSWORD AND CONFIRM PASSWORD NOT EQUAL");
+//            alert1.showAndWait(); }
+
+         else if (employee_number.getText().trim().isEmpty() || employee_number.getText().trim().length() != 10) {
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setContentText("ENTER VALID MOBILE NUMBER");
+            alert1.showAndWait(); }
+
+//         else if (Empaddress.getText().trim().isEmpty()) {
+//            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+//            alert1.setContentText("PLEASE ENTER ADDRESS");
+//            alert1.showAndWait();
+//        }
+
+         else if (employee_found == true) {
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setContentText("USERNAME ALREADY EXIST TRY WITH OTHER USERNAME");
+            alert1.showAndWait();
+        } else {
+
+            try {
+
+                int i = 0, j = 0;
+
+                String query1 = "Insert into employee(e_id, e_name, username, password, " +
+                        "e_pnumber) values(?,?,?,?,?)";
+                PreparedStatement preparedStatement1 = connection.prepareStatement(query1);
+                preparedStatement1.setString(1, employee_id.getText().trim());
+                preparedStatement1.setString(2, employee_name.getText().trim());
+                preparedStatement1.setString(3, employee_name.getText().trim());
+                preparedStatement1.setString(4, password.getText().trim());
+                preparedStatement1.setString(5, employee_number.getText().trim());
+
+
+                i=preparedStatement1.executeUpdate();
+
+                String query2 = "Insert into login(username,password,login_type) values(?,?,?)";
+                PreparedStatement preparedStatement2 = connection.prepareStatement(query2);
+
+                preparedStatement2.setString(1, employee_name.getText().trim());
+                preparedStatement2.setString(2, password.getText().trim());
+                preparedStatement2.setString(3, "employee");
+
+                j = preparedStatement2.executeUpdate();
+
+                if (i > 0 && j > 0) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("EMPLOYEE INFORMATION ADDED SUCCESSFULLY");
+                    alert.showAndWait();
+
+                    refresh_employee(); }
+                else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("ERROR IN SAVING DATA");
+                    alert.showAndWait(); }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (connection != null) {
+                        connection.close(); }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } } } }
+
+                void refresh_employee(){
+                    employee_id.clear();
+                    employee_name.clear();
+                    employee_number.clear();
+                    password.clear();
+                }
+
+
+
+    public void view_emp(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("employee.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        System.out.println("Employee details");
+        Stage stage = new Stage();
+        stage.setTitle("emp_details");
+        stage.setScene(new Scene(root1, 900, 700));
+        stage.show();
+
+    }
+
+    public void gen_reg_rep(ActionEvent actionEvent) {
+        view();
+    }
+
+    public void view()
+    {
+        String query="select * from employee";
+        try {
+            LoadingTableViewDataSelectedRowName.Welcome(query, view_reg_pane,250,950);
+            edit_pane.setVisible(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void employeeReprtPDF(ActionEvent actionEvent) throws Exception {
+
+        String query = "SELECT * FROM employee";
+
+        File dir = new File(Check.drive_name()+Configuring_Path.FOLDER_PATH);
+        dir.mkdir();
+        File dir2=new File(Check.drive_name()+Configuring_Path.FOLDER_PATH+"EMPLOYEE_REPORT");
+        dir2.mkdir();
+        String path=Check.drive_name()+Configuring_Path.FOLDER_PATH+"/EMPLOYEE_REPORT/Employee_report.csv";
+
+        Controller.export_excel(query,path);
+    }
+
+    public void edit_employee(ActionEvent actionEvent) {
+    }
+
+    public void delete_employee(ActionEvent actionEvent) {
+    }
+
+    public void save_employee(ActionEvent actionEvent) {
     }
 }
